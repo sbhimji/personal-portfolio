91a4d1747e38860a2546acbb0254b4d9d1a7cd4d
Saim Bhimji <bhimjisaim@gmail.com>
2024-05-26 13:38:11 -0400

    switched jumbotron

--- src/main.js
+++ src/main.js
@@ -24,20 +24,42 @@ const loader = new GLTFLoader();
 //load model, add to scene
 loader.load( '../public/models/court.glb', function ( gltf ) {
 	console.log(gltf.scene);
-	scene.add( gltf.scene );
-
+	
 	//color of all court lines
 	gltf.scene.getObjectByName('basketball_court').children[0].children[0].material.color.setHex(0x000000);
-	//color of jumbotrons
-	gltf.scene.children[3].material.color.setHex(0xFFFFFF);
-	gltf.scene.getObjectByName('Cylinder').material.color.setHex(0xFFFFFF);
+
+
+	const textureLoader = new THREE.TextureLoader();
+	textureLoader.setPath('../public/textures/');
+	const texture = textureLoader.load('test/space_jam.png');
+	const blankTexture = textureLoader.load('test/black.png');
+	const materialArray = [
+		new THREE.MeshStandardMaterial( {color:0xffffff, map:texture} ), 
+		new THREE.MeshStandardMaterial( {color:0xffffff, map:texture} ), 
+		new THREE.MeshStandardMaterial( {color:0xffffff, map:blankTexture} ), 
+		new THREE.MeshStandardMaterial( {color:0xffffff, map:texture} ),
+		new THREE.MeshStandardMaterial( {color:0xffffff, map:texture} ),
+		new THREE.MeshStandardMaterial( {color:0xffffff, map:texture} )
+	];
 	
+	let cubeGeometry = new THREE.BoxGeometry(27,27,27);	
+	
+	let cubeMesh = new THREE.Mesh( cubeGeometry, materialArray );
+	cubeMesh.position.set(0, 40, 0);
+	scene.add(cubeMesh);
+
+
+	scene.add( gltf.scene );
+
+	console.log(scene);
+	animate(renderer, scene, camera);
 }, undefined, function ( error ) {
 	console.error( error );
 } );
 
-//animate(renderer, scene, camera, controls);
-animate(renderer, scene, camera);
+//animate(renderer, scene, camera);
+
+
 
 
 
